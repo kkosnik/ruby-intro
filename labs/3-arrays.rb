53a439386d71bfce738c50866515beaea6b0fe99
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-01-25 16:24:59 +0000

    arrays lab

--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -14,3 +14,9 @@
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+my_list = ["milk", "eggs", "bacon"]
+friend_list = ["beer", "cookies", "apples", "bacon"]
+combined_list = my_list + friend_list
+
+puts combined_list.sort.uniq
