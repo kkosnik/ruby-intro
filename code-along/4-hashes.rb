278dc1843af04eb086ac35d4c9587bd19c6a6a8d
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-01-25 16:55:05 +0000

    finished hashes code along

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,34 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name: "Kelly",
+    location: "Evanston",
+    status: "Student"
+}
 
 # Accessing data from the hash
+puts profile
+puts profile[:name]
+puts profile[:location]
+puts profile[:status]
 
 # More Complex Hashes
+profile = {
+    name: "Kelly",
+    location: {
+        city: "Evanston",
+        state: "IL"
+    },  
+        status: "Student",
+        timeline: [
+          {status: "in class", posted: "8:30am"},
+          {status: "eating tacos", posted: "11am"}
+    ]
+}
+
+# Write city
+puts profile[:location][:city]
+
+# Write status 2
+puts profile[:timeline][0][:status]
